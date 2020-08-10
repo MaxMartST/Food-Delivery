e043b393018385a36bbba907ca2c48466901f0c9
Max <addergtx@gmail.com>
2020-08-10 14:07:31 +0300

    add oop for menu card

--- js/script.js
+++ js/script.js
@@ -95,14 +95,17 @@ window.addEventListener('DOMContentLoaded', () => {
 
     setClock('.timer', deadline);
 
-    //Modal
+    //Modal and timeout modal
     const modalTrigger = document.querySelectorAll('[data-modal]'),
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-close]');
+    
+    //const modalTimerId = setTimeout(openModel, 5000);
 
     function openModel() {
         modal.classList.toggle('show');
         document.body.style.overflow = 'hidden';
+        //clearInterval(modalTimerId);
     }
 
     function closeModal() {
@@ -117,7 +120,7 @@ window.addEventListener('DOMContentLoaded', () => {
     //отработка нажатия на Esc для закрытия мод. окна
     modalCloseBtn.addEventListener('click', closeModal);
     document.addEventListener('keydown', (event) => {
-        if (event.code === 'Escape' && modal.classList.contains('show')) {
+        if (event.code === 'Escape' && modal.classList.contains('show')){
             closeModal();
         }
     });
@@ -128,4 +131,76 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     });
+
+    function showModalByScroll () {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModel();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    //Metrics: modalByScroll
+    window.addEventListener('scroll', showModalByScroll);
+
+    //add class of menu
+    class MenuCard {
+        constructor (src, alt, title, descr, price, parentSelector) {
+             this.src = src;
+             this.alt = alt;
+             this.title = title;
+             this.descr = descr;
+             this.price = price;
+             this.parent = document.querySelector(parentSelector);//передаём родителя
+             this.transfer = 27;
+             this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');//создаём динамический элемент
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);//добавляем динамический элемент
+        }
+    }
+
+    new MenuCard(
+        'img/tabs/vegy.jpg',
+        'vegy',
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        'img/tabs/elite.jpg',
+        'elite',
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        14,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        'img/tabs/post.jpg',
+        'post',
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        21,
+        '.menu .container'
+    ).render();
 });
